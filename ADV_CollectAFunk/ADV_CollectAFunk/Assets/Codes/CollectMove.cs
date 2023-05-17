a30141f252e0856e5884f4019802bef13ed79fb8
Akira <jalpi.ap@outlook.es>
2023-05-17 11:51:37 +0200

    Bug Fixed (Double Coins)

---
+++ ADV_CollectAFunk/ADV_CollectAFunk/Assets/Codes/CollectMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectMove : MonoBehaviour
+{
+    public float amplitude = 0.2f; // La amplitud del movimiento
+    public float speed = 0.5f; // La velocidad del movimiento
+    private float startYPos; // La posición Y inicial del objeto
+    // Start is called before the first frame update
+    void Start()
+    {
+        startYPos = transform.position.y; // Guardamos la posición Y inicial del objeto
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(0f, Time.time * 100f, 0);
+        // Calculamos la posición Y en función del tiempo
+        float yPos = startYPos + amplitude * Mathf.Sin(speed * Time.time);
+
+        // Creamos un nuevo vector de posición con la nueva coordenada Y
+        Vector3 newPosition = new Vector3(transform.position.x, yPos, transform.position.z);
+
+        // Actualizamos la posición del objeto
+        transform.position = newPosition;
+    }
+}
